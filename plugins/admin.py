48a2c9b62554ebc85d90cc889829a5646742df1d
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2024-01-22 22:37:17 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,7 +2,6 @@ from pyrogram.types import (
     InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
 import os
 from pyrogram import Client, filters
-from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
 ADMIN = int(os.environ.get("ADMIN", ))
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
